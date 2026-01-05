b88ae971c5e4f1bbd46cb95c430bc6fed266a62f
Amir Idlan <amiridlan1999@gmail.com>
2026-01-05 15:39:50 +0800

    actually filled up the migration file

--- database/migrations/2026_01_04_080610_add_image_to_event_spaces_table.php
+++ database/migrations/2026_01_04_080610_add_image_to_event_spaces_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('event_spaces', function (Blueprint $table) {
-            //
+            $table->string('image')->nullable()->after('id');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('event_spaces', function (Blueprint $table) {
-            //
+            $table->dropColumn('image');
         });
     }
 };
